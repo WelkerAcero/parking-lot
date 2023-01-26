0b1388edd98ae11e4d25505f2b44dcb0d4d75244
Anggie2020 <castellanos.anggie17@gmail.com>
2023-01-26 13:25:02 -0500

    controller and login view are created

---
+++ resources/views/login_views/login.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.template')
+
+@section('title', 'Login')
+
+@section('content')
+    <div>
+        <div>
+            <h1>SISTEMA DE CONTROL DE ACCESO</h1>
+            <img src="{{asset('img/icons/car-side-solid.svg')}}" width="50px">
+        </div>
+        <div>
+            <img src="{{asset('img/parking.jpg')}}" width="500px">
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', [SessionController::class, 'index']);
+
+
+
