68ca91ac2240a3c9a2680de7b93db0aa5d3c3a9a
Anggie2020 <castellanos.anggie17@gmail.com>
2023-01-27 22:58:16 -0500

    estructura del login lista

--- resources/views/login_views/login.blade.php
+++ resources/views/login_views/login.blade.php
@@ -3,13 +3,27 @@
 @section('title', 'Login')
 
 @section('content')
-    <div>
-        <div>
-            <h1>SISTEMA DE CONTROL DE ACCESO</h1>
-            <img src="{{asset('img/icons/car-side-solid.svg')}}" width="50px">
+    <div class="container-main-login">
+        {{-- PART LEFT --}}
+        <div class="login-part-left">
+            <img src="{{ asset('img/Logo.png') }}" width="150px">
+            <h1 class="login-left-title">SISTEMA DE CONTROL DE ACCESO</h1>
+            <img src="{{ asset('img/icons/car-side-solid.svg') }}" width="50px">
+
+            <form action="" method="post"> <br>
+                <input class="form-control" type="email" id="email" name="email"
+                    placeholder="Escriba su correo" /><br>
+
+                <input type="password" class="form-control" id="password" name="password"
+                    placeholder="Escriba su contraseña" /><br><br>
+
+                <button class="form-control login-left-btn" type="submit">Acceder</button>
+            </form>
         </div>
-        <div>
-            <img src="{{asset('img/parking.jpg')}}" width="500px">
+
+        {{-- PART RIGHT --}}
+        <div class="login-part-right">
+            <img class="login-img-right" src="{{ asset('img/parking.jpg') }}">
         </div>
     </div>
 @endsection
