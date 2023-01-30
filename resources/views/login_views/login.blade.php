7484ffd9708e75aca5f6756bd34fd440f1be333f
Anggie2020 <castellanos.anggie17@gmail.com>
2023-01-29 20:18:43 -0500

    design and responsive of login done

--- resources/views/login_views/login.blade.php
+++ resources/views/login_views/login.blade.php
@@ -4,43 +4,51 @@
 
 @section('content')
     <div class="container-main-login">
-        {{-- PART LEFT --}}
-        <div class="login-part-left">
-            <img src="{{ asset('img/Logo.png') }}" width="150px">
-            <h1 class="login-left-title">SISTEMA DE CONTROL DE ACCESO</h1>
-            <img src="{{ asset('img/icons/car-side-solid.svg') }}" width="50px">
+        <div class="container-login">
 
-            <div>
-                <p style="color: red; text-align:center;">{{ $msgErr ?? '' }}</p>
+            {{-- PART LEFT --}}
+            <div class="login-part-left">
+
+                <img src="{{ asset('img/Logo.png') }}" width="150px">
+                <h1 class="login-left-title"><b>SISTEMA DE CONTROL DE ACCESO</b></h1>
+                <img src="{{ asset('img/icons/car-side-solid.svg') }}" width="50px">
+
+
+                <div class="login-contain-form">
+                    <p class="login-error">{{ $msgErr ?? '' }}</p>
+                </div>
+
+                <form action="{{ route('validate.user') }}" method="post">
+                    <br><br>
+                    <h2 class="login-form-text">Por favor ingrese sus credenciales</h2>
+                    @csrf
+                    <div class="login-contain-form">
+                        <label class="login-form" for="email" style="color:white">Email</label>
+                        <input class="form-control" type="email" id="email" name="email"
+                            placeholder="Escriba su correo" />
+                        @error('email')
+                            <p class="login-error">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    <br>
+                    <div class="login-contain-form">
+                        <label class="login-form" for="password" style="color:white">Contraseña</label>
+                        <input type="password" class="form-control" id="password" name="password"
+                            placeholder="Escriba su contraseña" />
+                        @error('password')
+                            <p class="login-error">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    <br><br>
+                    <button class="form-control login-left-btn" type="submit">Acceder</button>
+                </form>
             </div>
 
-            <form action="{{ route('validate.user') }}" method="post"> <br>
-                @csrf
-                <div>
-                    <label for="email" style="color:white">Email</label>
-                    <input class="form-control" type="email" id="email" name="email"
-                        placeholder="Escriba su correo" />
-                    @error('email')
-                        <p style="color: red">{{ $message }}</p>
-                    @enderror
-                </div>
-                <br>
-                <div>
-                    <label for="password" style="color:white">Password</label>
-                    <input type="password" class="form-control" id="password" name="password"
-                        placeholder="Escriba su contraseña" /><br>
-                    @error('password')
-                        <p style="color: red">{{ $message }}</p>
-                    @enderror
-                </div>
-                <br>
-                <button class="form-control login-left-btn" type="submit">Acceder</button>
-            </form>
-        </div>
+            {{-- PART RIGHT --}}
+            <div class="login-part-right">
+                <img class="login-img-right" src="{{ asset('img/parking.jpg') }}">
+            </div>
 
-        {{-- PART RIGHT --}}
-        <div class="login-part-right">
-            <img class="login-img-right" src="{{ asset('img/parking.jpg') }}">
         </div>
     </div>
 @endsection
