b7c198e954f5e790fe24d8dcafd632ba556a3792
Anggie2020 <castellanos.anggie17@gmail.com>
2023-02-06 14:10:34 -0500

    structure of form create customer

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,10 +31,10 @@
                                     d="M272 304h-96C78.8 304 0 382.8 0 480c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32C448 382.8 369.2 304 272 304zM48.99 464C56.89 400.9 110.8 352 176 352h96c65.16 0 119.1 48.95 127 112H48.99zM224 256c70.69 0 128-57.31 128-128c0-70.69-57.31-128-128-128S96 57.31 96 128C96 198.7 153.3 256 224 256zM224 48c44.11 0 80 35.89 80 80c0 44.11-35.89 80-80 80S144 172.1 144 128C144 83.89 179.9 48 224 48z" />
                             </svg>
                         </div>
-                        <h3 class="module-top-text"><b>Gestionar usuarios</b></h3>
+                        <h3 class="module-top-text"><b>Gestionar personas</b></h3>
                     </div>
                     <div class="module-bottom-text">
-                        <h4 style="margin-right: 23px">Ir a módulo de usuarios</h4>
+                        <h4 style="margin-right: 23px">Ir a módulo de personas</h4>
                         <img class="dash-icon-acceder" src="{{ asset('img/icons/enter.svg') }}" width="18px">
                     </div>
                 </a>
