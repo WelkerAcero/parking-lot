d638e7ec223a04956c1d516f74800027930621fa
WelkerAcero <universityestudiosca@gmail.com>
2023-02-13 22:41:18 -0500

    Route - login on home URL

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::controller(SessionController::class)->group(function () {
+    Route::get('/', 'index')->name('login');
     Route::get('/login', 'index')->name('login');
     Route::post('/login', 'authentication')->name('validate.user');
     Route::post('/logout', 'logout')->name('logout');
